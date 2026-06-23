97502b4d1cb5d39b589b661dbadc46737e0bbabe
Yonatan Israeli <yonatanreuvenisraeli@gmail.com>
2026-06-22 20:20:19 -0400

    Update Secure Boot Updater.bat

--- Secure Boot Updater.bat
+++ Secure Boot Updater.bat
@@ -40,7 +40,9 @@ if exist "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boo
 "%windir%\System32\reg.exe" add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Secureboot" /v AvailableUpdates /t REG_DWORD /d 0x5944 /f > nul 2>&1
 "%windir%\System32\schtasks.exe" /run /tn "\Microsoft\Windows\PI\Secure-Boot-Update" > nul 2>&1
 (echo "%windir%\System32\schtasks.exe" /run /tn "\Microsoft\Windows\PI\Secure-Boot-Update" ^> nul 2^>^&1) > "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo Secure Boot has been updated. Press any key to close this batch file.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
 (echo start "" "%windir%\System32\cmd.exe" /c del "%~f0" /f /q ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo exit) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
 echo.
 echo Restart needed to finish updating Secure Boot. Please save everything you want before restarting this PC! Press any key to restart this PC.
 pause > nul 2>&1
