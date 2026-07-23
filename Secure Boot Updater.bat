c00f8d30613ef39591f60b8fe82f9a6a5ac4a0cb
Yonatan Israeli <yonatanreuvenisraeli@gmail.com>
2026-07-23 14:18:25 -0400

    Update Secure Boot Updater.bat

--- Secure Boot Updater.bat
+++ Secure Boot Updater.bat
@@ -11,7 +11,7 @@ echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli
 if not "%errorlevel%"=="0" goto "NotAdministrator"
 "%windir%\System32\net.exe" user > nul 2>&1
 if not "%errorlevel%"=="0" goto "InPERE"
-goto "Start"
+goto "SecureBootSet"
 
 :"NotAdministrator"
 echo.
@@ -29,11 +29,38 @@ goto "Close"
 endlocal
 exit
 
+:"SecureBootSet"
+set SecureBootExist=
+goto "Start"
+
 :"Start"
+if exist "SecureBoot.txt" goto "SecureBootExist"
 echo.
-echo Press any key to update Secure boot.
+echo Checking Secure boot status.
+"%windir%\System32\reg.exe" query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecureBoot\Servicing" /v "UEFICA2023Status" | "%windir%\System32\find.exe" /i "UEFICA2023Status" > "SecureBoot.txt"
+set /p SecureBoot=< "SecureBoot.txt"
+del "SecureBoot.txt" /f /q > nul 2>&1
+if /i "%SecureBootExist%"=="True" echo.
+if /i "%SecureBootExist%"=="True" echo You can now rename or move the file back to "SecureBoot.txt". Press any key to continue.
+if /i "%SecureBootExist%"=="True pause > nul 2>&1
+if /i "%SecureBootExist%"=="True" echo.
+if /i "%SecureBoot%"=="    UEFICA2023Status    REG_SZ    Updated" goto "AlreadyUpdated"
+if /i "%SecureBootExist%"=="" echo Secure boot is not updated. Press any key to update Secure boot.
 pause > nul 2>&1
 goto "Update"
+
+:"SecureBootExist"
+set SecureBootExist=True
+echo.
+echo Please temporarily rename to something else or temporarily move to another location "SecureBoot.txt" in order for this batch file to proceed. "SecureBoot.txt" is not a system file. "SecureBoot.txt" is located in the folder "%cd%". Press any key to continue when "SecureBoot.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.
+pause > nul 2>&1
+goto "Start"
+
+:"AlreadyUpdated"
+echo Secure boot is already updated. Press any key to close this batch file.
+pause > nul 2>&1
+endlocal
+exit
 
 :"Update"
 if exist "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure boot Updater.bat" goto "Exist"
@@ -44,12 +71,34 @@ echo Updating Secure boot.
 (echo @echo off) > "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
 (echo title Secure Boot Updater) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
 (echo setlocal) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
-(echo "%windir%\System32\schtasks.exe" /run /tn "\Microsoft\Windows\PI\Secure-Boot-Update" ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
-(echo echo Secure boot has been updated. Press any key to close this batch file.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
-(echo start "" /min "%windir%\System32\cmd.exe" /c del "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat" /f /q ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo "%%windir%%\System32\schtasks.exe" /run /tn "\Microsoft\Windows\PI\Secure-Boot-Update" ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo goto "SecureBootSet") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo :"SecureBootSet") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo set SecureBootExist=) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo goto "UpdateCheck") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo :"UpdateCheck") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo if exist "SecureBoot.txt" goto "SecureBootExist") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo "%%windir%%\System32\reg.exe" query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecureBoot\Servicing" /v "UEFICA2023Status" ^| "%%windir%%\System32\find.exe" /i "UEFICA2023Status" ^> "SecureBoot.txt") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo set /p SecureBoot=^< "SecureBoot.txt") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo del "SecureBoot.txt" /f /q ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo if /i "%%SecureBootExist%%"=="True" echo.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo if /i "%%SecureBootExist%%"=="True" echo You can now rename or move the file back to "SecureBoot.txt". Press any key to continue.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo if /i "%%SecureBootExist%%"=="True" pause ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo if /i "%%SecureBootExist%%"=="True" echo.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo if /i "%%SecureBoo%%"=="    UEFICA2023Status    REG_SZ    Updated" echo Secure boot has been updated. Press any key to close this batch file.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo if /i not "%%SecureBoo%%"=="    UEFICA2023Status    REG_SZ    Updated" echo There has been an error and Secure boot has not been updated. Press any key to close this batch file.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo start "" /min "%%windir%%\System32\cmd.exe" /c del "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat" /f /q ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
 (echo pause ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
 (echo endlocal) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
 (echo exit) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo :"SecureBootExist") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo set SecureBootExist=True) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo echo Please temporarily rename to something else or temporarily move to another location "SecureBoot.txt" in order for this batch file to proceed. "SecureBoot.txt" is not a system file. "SecureBoot.txt" is located in the folder "%%cd%%". Press any key to continue when "SecureBoot.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo pause ^> nul 2^>^&1) >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
+(echo goto "UpdateCheck") >> "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\Secure Boot Updater.bat"
 echo Restart needed to finish updating Secure boot. Please save everything you want before restarting this PC! Press any key to restart this PC.
 pause > nul 2>&1
 "%windir%\System32\shutdown.exe" /r /t 0
